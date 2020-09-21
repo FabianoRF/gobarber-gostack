8d24fb788794e1cc7bcde840b575ecf365373551
FabianoRF <freis884@gmail.com>
2020-09-21 17:05:03 -0300

    implemented DDD architecture

--- gobarber-server/src/modules/users/infra/http/users.routes.ts
+++ gobarber-server/src/modules/users/infra/http/users.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import multer from 'multer';
-import uploadConfig from '../config/upload';
 
-import CreateUserService from '../services/CreateUserService';
-import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+import uploadConfig from '@config/upload';
+import CreateUserService from '@modules/users/services/CreateUserService';
 
-import ensureAutheticated from '../middlewares/ensureAutheticated';
+import ensureAutheticated from '@modules/users/infra/middlewares/ensureAutheticated';
+import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
@@ -38,6 +38,7 @@ usersRouter.patch(
       avatarFilename: request.file.filename,
     });
 
+    // @ts-ignore
     delete user.password;
 
     return response.json(user);
--- gobarber-server/src/shared/infra/http/server.ts
+++ gobarber-server/src/shared/infra/http/server.ts
@@ -4,11 +4,11 @@ import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
 
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
 import routes from './routes';
-import uploadConfig from './config/upload';
-import AppError from './errors/AppError';
 
-import './database';
+import '@shared/infra/typeorm';
 
 const app = express();
 
@@ -35,5 +35,5 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
 });
 
 app.listen(3333, () => {
-  console.log('Server start on port 3333!');
+  console.log('*** Server start on port 3333!');
 });
