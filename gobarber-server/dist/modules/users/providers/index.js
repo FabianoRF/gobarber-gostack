2fea176298ab2947c6f7df5197c334e273a77fa4
FabianoRF <freis884@gmail.com>
2020-12-02 22:03:48 -0300

    correcoes

--- gobarber-server/dist/modules/users/providers/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _tsyringe = require("tsyringe");
-
-var _BCriptyHashProvider = _interopRequireDefault(require("./HashProvider/implementations/BCriptyHashProvider"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-_tsyringe.container.registerSingleton('HashProvider', _BCriptyHashProvider.default);
